7c2f0fcdd43f612b99c0a6e4e14026b003454ac0
Borna <thetdm90@gmail.com>
2023-07-08 23:32:29 +0200

    deleted some stuff

--- scripts/main_character.gd
+++ scripts/main_character.gd
@@ -2,22 +2,26 @@ extends StaticBody2D
 
 var bullets_left = [3,3,3]
 
+@onready var firing_point = get_parent().get_node("FiringPoint")
+
 const bullet_path = preload("res://scenes/bullet.tscn")
 # shouldn't preload here, but who cares (not me)
 const red_bullet = preload("res://assets/img/red-temp.png")
 const blue_bullet = preload("res://assets/img/blue-temp.png")
 const silver_bullet = preload("res://assets/img/silver-temp.png")
 var is_fired = false
+var finished = true
 var active_bullet = ""
 var active_index = -1
 
 
 func _input(event):
-	if Input.is_action_just_pressed("shoot") and !is_fired and !$SwapMenu.visible and active_bullet != "" and bullets_left[active_index] > 0:
+	if Input.is_action_just_pressed("shoot") and !is_fired and !$SwapMenu.visible and active_bullet != "" and bullets_left[active_index] > 0 and finished:
 		is_fired = true
 		var bullet = bullet_path.instantiate()
-		bullet.position = $FiringPoint.position
+		bullet.position = firing_point.position
 		$AnimationPlayer.play("shoot")
+		finished = false
 		await get_tree().create_timer(0.7).timeout
 		get_parent().add_child(bullet)
 		$Camera2D.enabled = false
@@ -38,6 +42,7 @@ func _input(event):
 			1: $SwapMenu/BlueCount.text = str(bullets_left[active_index])
 			2: $SwapMenu/SilverCount.text = str(bullets_left[active_index])
 		await $AnimationPlayer.animation_finished
+		finished = true
 		$AnimationPlayer.play("idle")
 
 	if Input.is_action_pressed("swap") and !is_fired:
